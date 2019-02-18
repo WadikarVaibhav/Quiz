8c11aa1bbfa9595d2cc80051bc0b0967f0218d77
WadikarVaibhav <vaibhavwadikar8@gmail.com>
2019-02-18 00:15:48 -0800

    Established database connection to add new user

--- app/src/main/java/com/vaibhav/quiz/MainActivity.kt
+++ app/src/main/java/com/vaibhav/quiz/MainActivity.kt
@@ -2,11 +2,21 @@ package com.vaibhav.quiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var context = this
+
+        submit.setOnClickListener {
+            var user = User(firstname.text.toString(), lastname.text.toString(), nickname.text.toString(), age.text.toString().toInt())
+            var db = DatabaseOpenHelper(context)
+            db.insert(user)
+        }
+
     }
 }
